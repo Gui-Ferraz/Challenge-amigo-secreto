6d09eba1fb596042e4e79cf2bb142ae242e53a3b
Gui-Ferraz <gm.ferraz99@gmail.com>
2025-03-18 16:35:57 -0300

    Funcao adicionarLista concluida

--- app.js
+++ app.js
@@ -12,4 +12,18 @@ function adicionarAmigo() {
     amigos.push(name);
     campo = document.querySelector('input');
     campo.value = '';
+
+    adicionarLista();
+}
+
+function adicionarLista() {
+    let lista = document.querySelector('ul');
+    lista.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; ++i) {
+        let novoAmigo = document.createElement('li');   // Cria um <li>
+        novoAmigo.textContent = amigos[i];              // Define o texto do item
+        lista.appendChild(novoAmigo);                   // Insere o <li> em <ul>
+        console.log(i);
+    }
 }
